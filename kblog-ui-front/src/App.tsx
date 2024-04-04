d73ebeb6a1127f577b1d47f3efb07f63e84882ae
快乐火猫 <cx282694@gmail.com>
2024-04-04 20:14:48 +0800

    分割组件。

--- kblog-ui-front/src/App.tsx
+++ kblog-ui-front/src/App.tsx
@@ -1,24 +1,19 @@
 import "./App.css";
 
-import { BrowserRouter, NavLink } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 
-import RouterConfig from "./routers/RouterConfig";
 import { Fragment } from "react/jsx-runtime";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainView from "./components/MainView";
 
 function App() {
     return (
         <Fragment>
             <BrowserRouter>
-                <nav>
-                    <NavLink to="/home">Home</NavLink> |
-                    <NavLink to="/about">About</NavLink> |
-                    <NavLink to={{pathname:"/about", hash:"#256", search:"a=18&n=b"}} state={{'g':'n'}}>About-Object</NavLink> |
-                    <NavLink to="/wallet">Wallet</NavLink> |
-                    <NavLink to="/test1">Test1</NavLink> |
-                    <NavLink to="/test/abc2">Test2</NavLink> |
-                    <NavLink to="/admin">Admin</NavLink>
-                </nav>
-                <RouterConfig></RouterConfig>
+                <Header></Header>
+                <MainView></MainView>
+                <Footer></Footer>
             </BrowserRouter>
         </Fragment>
     );
---
+++ kblog-ui-front/src/components/MainView.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import RouterConfig from '../routers/RouterConfig';
+
+const MainView: React.FC = () => {
+    return (
+        <Fragment>
+            <RouterConfig />
+        </Fragment>
+    );
+};
+
+export default MainView;
