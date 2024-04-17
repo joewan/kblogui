adb750749c3580c2974b1864491077da6b117273
快乐火猫 <cx282694@gmail.com>
2024-04-17 16:55:20 +0800

    antd admin.

--- kblog-ui-front/src/App.tsx
+++ kblog-ui-front/src/App.tsx
@@ -1,25 +1,17 @@
-import "./App.css";
-import 'bulma/css/bulma.min.css';
-
 import { Component } from 'react';
-import { BrowserRouter} from "react-router-dom";
-
 import { Fragment } from "react/jsx-runtime";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import MainView from "./components/MainView";
+import MyLayout from './components/MyLayout';
 
 class App extends Component {
     render() {
         return (
             <Fragment>
-                <BrowserRouter>
-                    <Header></Header>
-                    <MainView>
-
-                    </MainView>
-                    <Footer></Footer>
-                </BrowserRouter>
+                <MyLayout>
+                    <h1>田园科技博客</h1>
+                    <h2>田园科技博客</h2>
+                    <h3>田园科技博客</h3>
+                    <h4>田园科技博客</h4>
+                </MyLayout>
             </Fragment>
         );
     }
--- kblog-ui-front/src/main.tsx
+++ kblog-ui-front/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import 'antd/dist/reset.css';
 import ReactDOM from 'react-dom/client'
+import zhCN from 'antd/lib/locale/zh_CN'
 import App from './App.tsx'
 import './index.css'
+import { ConfigProvider } from 'antd'
+import { Fragment } from 'react/jsx-runtime'
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Fragment>
+    <Router>
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
+    </Router>
+  </Fragment>
 )
