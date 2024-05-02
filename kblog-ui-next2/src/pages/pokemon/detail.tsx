5557c86569c0c78d174af1fa619500ac67ab8a1c
bruce002 <bruce@goerlis.com>
2024-05-02 17:00:28 +0800

    Nextjs Pages Router API

---
+++ kblog-ui-next2/src/pages/pokemon/detail.tsx
@@ -0,0 +1,19 @@
+
+'use client';
+
+import React, { Component } from 'react';
+
+class PokemonDetail extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Detail</h2>
+                <h1 className="text-4xl font-bold underline">
+                    Pokemon Detail
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default PokemonDetail;
