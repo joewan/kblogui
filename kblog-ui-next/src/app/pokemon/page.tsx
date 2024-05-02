bdbee844f70b46defc6a766b9f97211ba29ee65a
bruce002 <bruce@goerlis.com>
2024-05-02 14:13:56 +0800

    src

---
+++ kblog-ui-next/src/app/pokemon/page.tsx
@@ -0,0 +1,16 @@
+
+'use client';
+
+import React, { Component } from 'react';
+
+class Pokemon extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Pokemon</h1>
+            </div>
+        );
+    }
+}
+
+export default Pokemon;
