7529c02603c4b5f799e589fb99a0acbd67722282
bruce002 <bruce@goerlis.com>
2024-05-03 21:48:32 +0800

    fix next config: transpilePackages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
+  transpilePackages: [
+    '@ant-design',
+    'rc-picker',
+    'rc-pagination',
+    'rc-util',
+    'antd',
+  ],
+}
 export default nextConfig;
