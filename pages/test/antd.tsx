66f46ad01c21e3bf576ddb6e05718fb6ce56194d
bruce002 <bruce@goerlis.com>
2024-05-03 21:51:34 +0800

    antd test

---
+++ pages/test/antd.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, Card, Image } from 'antd';
+
+const Home = () => (
+  <div className="App">
+    <Card>
+        <Image src='/vercel.svg'></Image>
+        <Button type="primary">Button</Button>
+    </Card>
+  </div>
+);
+
+export default Home;
